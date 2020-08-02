3ac935eaad9110805ca1c14f800477b0444b4a4d
wb-wsc730020 <wb-wsc730020@alibaba-inc.com>
2020-08-02 23:18:53 +0800

    更新项目

---
+++ springboot-02-config-autoconfig/src/main/java/com/alibaba/springboot/SpringbootConfig02AutoconfigApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfig02AutoconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConfig02AutoconfigApplication.class, args);
+	}
+
+}
