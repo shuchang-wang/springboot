3b17752d7522cb09940606bea8d2ac0834b53c4e
wb-wsc730020 <wb-wsc730020@alibaba-inc.com>
2020-08-02 18:47:18 +0800

    springboot 启动会扫描以下位置的application.properties或者application.yml文件作为Spring boot的默认配置文件

---
+++ springboot-config02/src/main/java/com/alibaba/springbootconfig02/SpringbootConfig02Application.java
@@ -0,0 +1,13 @@
+package com.alibaba.springbootconfig02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfig02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConfig02Application.class, args);
+	}
+
+}
---
+++ springboot-config02/src/test/java/com/alibaba/springbootconfig02/SpringbootConfig02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.springbootconfig02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConfig02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
