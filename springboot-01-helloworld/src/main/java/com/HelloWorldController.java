c4d8a8d899aa3fe86a04d6498f415bdd49b4c6cb
wb-wsc730020 <wb-wsc730020@alibaba-inc.com>
2020-08-02 23:07:09 +0800

    修改项目名称

---
+++ springboot-01-helloworld/src/main/java/com/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+}
---
+++ springboot-01-helloworld/src/main/java/com/alibaba/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.alibaba.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+//    @RequestMapping("/hello")
+//    public String sayHello() {
+//        return "Hello World";
+//    }
+
+}
