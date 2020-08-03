3a13c981239bbfe55f6273876fd34f722666b7dc
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-08-03 23:03:59 +0800

    添加thymeleaf模板

--- springboot-04-web-restfulcrud/src/main/java/com/alibaba/springboot/controller/HelloController.java
+++ springboot-04-web-restfulcrud/src/main/java/com/alibaba/springboot/controller/HelloController.java
@@ -1,14 +1,23 @@
 package com.alibaba.springboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String sayHello() {
         return "Hello EveryBody!";
+    }
+
+    @RequestMapping("/success")
+    public String success() {
+        //classpath:/templates/success.html
+        return "success";
     }
 
 }
