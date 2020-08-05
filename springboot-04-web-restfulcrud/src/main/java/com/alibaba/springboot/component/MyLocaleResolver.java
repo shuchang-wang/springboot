7baa531fb13a751e6d7a2287e3ba08e82eed2a52
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-08-05 19:00:35 +0800

    登录页面国际化实现中英文切换

---
+++ springboot-04-web-restfulcrud/src/main/java/com/alibaba/springboot/component/MyLocaleResolver.java
@@ -0,0 +1,28 @@
+package com.alibaba.springboot.component;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+@Component
+public class MyLocaleResolver implements LocaleResolver {
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        Locale locale = Locale.getDefault();
+        String l = request.getParameter("l");
+        if (!StringUtils.isEmpty(l)) {
+            String[] split = l.split("_");
+            locale = new Locale(split[0], split[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+    }
+}
--- springboot-04-web-restfulcrud/src/main/java/com/alibaba/springboot/config/MyMVCConfig.java
+++ springboot-04-web-restfulcrud/src/main/java/com/alibaba/springboot/config/MyMVCConfig.java
@@ -1,8 +1,9 @@
 package com.alibaba.springboot.config;
 
+import com.alibaba.springboot.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -25,10 +26,15 @@ public class MyMVCConfig extends WebMvcConfigurerAdapter {
             @Override
             public void addViewControllers(ViewControllerRegistry registry) {
                 registry.addViewController("/").setViewName("login");
-                registry.addViewController("/login").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
             }
         };
         return adapter;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 
 }
