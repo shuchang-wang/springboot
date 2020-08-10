357be72602271e9073c97b3c7ebe79c743ed07a8
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-08-10 10:12:53 +0800

    外置Servlet容器使用

---
+++ springboot-04-web-jsp/src/main/java/com/alibaba/springboot/Springboot04WebJspApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04WebJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot04WebJspApplication.class, args);
+	}
+
+}
---
+++ springboot-04-web-jsp/src/main/webapp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wb-wsc730020
+  Date: 2020/8/10
+  Time: 10:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello JSP</h1>
+<a href="abc">abc</a>
+</body>
+</html>
