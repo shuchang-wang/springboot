2f5d59015d2b153578262c12f4b44dc2fa0aea86
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-08-12 21:39:26 +0800

    初始化mybatis项目bean对象

---
+++ springboot-05-data-mybatis/src/main/java/com/alibaba/springboot/bean/Employee.java
@@ -0,0 +1,97 @@
+package com.alibaba.springboot.bean;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private Integer id;
+    private String lastName;
+    private String email;
+    private Integer gender;
+    private Integer dId;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String lastName, String email, Integer gender, Integer dId) {
+        this.id = id;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.dId = dId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", gender=" + gender +
+                ", dId=" + dId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+
+        Employee employee = (Employee) o;
+
+        if (getId() != null ? !getId().equals(employee.getId()) : employee.getId() != null) return false;
+        if (getLastName() != null ? !getLastName().equals(employee.getLastName()) : employee.getLastName() != null) return false;
+        if (getEmail() != null ? !getEmail().equals(employee.getEmail()) : employee.getEmail() != null) return false;
+        if (getGender() != null ? !getGender().equals(employee.getGender()) : employee.getGender() != null) return false;
+        return getdId() != null ? getdId().equals(employee.getdId()) : employee.getdId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getLastName() != null ? getLastName().hashCode() : 0);
+        result = 31 * result + (getEmail() != null ? getEmail().hashCode() : 0);
+        result = 31 * result + (getGender() != null ? getGender().hashCode() : 0);
+        result = 31 * result + (getdId() != null ? getdId().hashCode() : 0);
+        return result;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Integer getdId() {
+        return dId;
+    }
+
+    public void setdId(Integer dId) {
+        this.dId = dId;
+    }
+}
