6f717272a453064b146b6d4ba0c1f420519b8870
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-08-16 20:17:24 +0800

    初始化spring-boot-starter-amqp项目搭建

---
+++ springboot-09-amqp/src/main/java/com/alibaba/springboot/Springboot09AmqpApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot09AmqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot09AmqpApplication.class, args);
+	}
+
+}
