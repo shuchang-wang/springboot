6a0cda4169ca8e51e1df88a6197391a776335944
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-08-16 21:03:01 +0800

    新增RabbitTemplate对消息的发送和接收以及消息的序列化

--- springboot-09-amqp/src/main/java/com/alibaba/springboot/Springboot09AmqpApplication.java
+++ springboot-09-amqp/src/main/java/com/alibaba/springboot/Springboot09AmqpApplication.java
@@ -3,6 +3,14 @@ package com.alibaba.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 自动配置
+ * 1、RabbitAutoConfiguration
+ * 2、又自动配置了连接工厂ConnectionFactory
+ * 3、RabbitProperties封装了RabbitMQ的配置
+ * 4、RabbitTemplate：给RabbitMQ发送和接收消息
+ * 5、AmqpAdmin：RabbitMQ系统管理功能组件
+ */
 @SpringBootApplication
 public class Springboot09AmqpApplication {
 
--- springboot-09-amqp/src/test/java/com/alibaba/springboot/Springboot09AmqpApplicationTests.java
+++ springboot-09-amqp/src/test/java/com/alibaba/springboot/Springboot09AmqpApplicationTests.java
@@ -1,16 +1,79 @@
 package com.alibaba.springboot;
 
+import com.alibaba.springboot.bean.Book;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springboot09AmqpApplicationTests {
 
+	@Autowired
+	RabbitTemplate rabbitTemplate;
+
+	/**
+	 * 1、单播（点对点）
+	 */
 	@Test
-	public void contextLoads() {
+	public void send() {
+		//Message需要自己构造一个；定义消息体内容和消息头
+		//rabbitTemplate.send(exchange,routingKey,message);
+
+		//object默认当成消息体，只需要传入要发送的对象，自动序列化发送给rabbitmq；
+		//rabbitTemplate.convertAndSend(exchange,routingKey,Object);
+		Map<String,Object> map = new HashMap<>();
+		map.put("msg","第一个消息");
+		map.put("data", Arrays.asList("helloworld",123,true));
+		//对象被默认序列化以后发送出去
+		//rabbitTemplate.convertAndSend("exchange.direct","alibaba.news",map);
+		rabbitTemplate.convertAndSend("exchange.direct","alibaba.news",new Book("西游记","吴承恩"));
 	}
 
+	/**
+	 * 接收数据
+	 */
+	@Test
+	public void receive(){
+		Object o = rabbitTemplate.receiveAndConvert("alibaba.news");
+		System.out.println(o.getClass());
+		System.out.println(o);
+	}
+
+	@Test
+	public void send02() {
+		rabbitTemplate.convertAndSend("exchange.fanout","",new Book("三国演义","罗贯中"));
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
