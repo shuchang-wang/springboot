19148712fbc390306150262857f9026549807afe
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-08-22 14:31:24 +0800

    springboot-security安全认证项目

--- springboot-12-security/src/main/java/com/alibaba/springboot/config/MySecurityConfig.java
+++ springboot-12-security/src/main/java/com/alibaba/springboot/config/MySecurityConfig.java
@@ -1,9 +1,9 @@
 package com.alibaba.springboot.config;
 
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+        import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * 1、引入SpringSecurity
