e9771cbb38fddc1885aeaf12b48fd5ae4b0c13f8
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-08-30 16:21:49 +0800

    ABA问题展示和解决

---
+++ interview-demo/src/main/java/com/alibaba/interview/study/thread/ABADemo.java
@@ -0,0 +1,73 @@
+package com.alibaba.interview.study.thread;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+/**
+ * @author: WSC
+ * @Create 2020/8/30 15:28
+ * <p>
+ * ABA问题
+ */
+public class ABADemo {
+
+    static AtomicReference<Integer> atomicReference = new AtomicReference<>(100);
+    static AtomicStampedReference<Integer> atomicStampedReference = new AtomicStampedReference<>(100, 1);
+
+    public static void main(String[] args) {
+        System.out.println("===============以下是ABA问题的产生===================");
+//        abaShow();
+        System.out.println("===============以下是ABA问题的解决===================");
+        abaSolve();
+    }
+
+    public static void abaSolve() {
+        new Thread(() -> {
+            int stamp = atomicStampedReference.getStamp();
+            try {
+                //暂停1秒钟
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "\t第1次版本号：" + stamp);
+            atomicStampedReference.compareAndSet(100, 101, stamp, stamp + 1);
+            System.out.println(Thread.currentThread().getName() + "\t第2次版本号：" + atomicStampedReference.getStamp());
+            atomicStampedReference.compareAndSet(101, 100, atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1);
+            System.out.println(Thread.currentThread().getName() + "\t第3次版本号：" + atomicStampedReference.getStamp());
+
+        }, "t3").start();
+
+        new Thread(() -> {
+            int stamp = atomicStampedReference.getStamp();
+            try {
+                System.out.println(Thread.currentThread().getName() + "\t第1次版本号：" + stamp);
+                //暂停3秒钟t4线程，保证上面的t3线程完成了ABA操作
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean result = atomicStampedReference.compareAndSet(100, 2020, stamp, stamp + 1);
+            System.out.println(Thread.currentThread().getName() + "\t修改是否成功：" + result + "\t当前最新实际版本号：" + atomicStampedReference.getStamp());
+            System.out.println(Thread.currentThread().getName() + "\t当前实际最新值：" + atomicStampedReference.getReference());
+        }, "t4").start();
+    }
+
+    public static void abaShow() {
+        new Thread(() -> {
+            atomicReference.compareAndSet(100, 101);
+            atomicReference.compareAndSet(101, 100);
+        }, "t1").start();
+
+        new Thread(() -> {
+            try {
+                //t2线程先暂停2秒，保证t1线程完成了一次ABA操作
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(atomicReference.compareAndSet(100, 2020) + "\t" + atomicReference.get());
+        }, "t2").start();
+    }
+}
---
+++ interview-demo/src/main/java/com/alibaba/interview/study/thread/AtomicReferenceDemo.java
@@ -0,0 +1,32 @@
+package com.alibaba.interview.study.thread;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author: WSC
+ * @Create 2020/8/30 14:00
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+class User {
+    private String name;
+    private int age;
+}
+
+public class AtomicReferenceDemo {
+    public static void main(String[] args) {
+        User zs = new User("zhangsan", 23);
+        User ls = new User("lisi", 24);
+        AtomicReference<User> userAtomicReference = new AtomicReference<>();
+        userAtomicReference.set(zs);
+        System.out.println(userAtomicReference.compareAndSet(zs, ls) + "\t" + userAtomicReference.get().toString());
+        System.out.println(userAtomicReference.compareAndSet(zs, ls) + "\t" + userAtomicReference.get().toString());
+    }
+}
