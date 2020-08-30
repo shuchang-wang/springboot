02c6149e510b5a8efbd2fc2ba05a493bbae84e57
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-08-30 21:32:51 +0800

    自己实现一个自旋锁demo

---
+++ interview-demo/src/main/java/com/alibaba/interview/study/thread/SpinLockDemo.java
@@ -0,0 +1,58 @@
+package com.alibaba.interview.study.thread;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author: WSC
+ * @Create 2020/8/30 21:08
+ * <p>
+ * 题目：实现一个自旋锁
+ * 自旋锁的好处：循环比较获取直到成功为止，没有类似wait的阻塞。
+ * <p>
+ * 通过CAS操作完成自旋锁，A线程先进来调用myLock方法自己持有5秒钟，B随后进来发现当前有线程持有锁，
+ * 不是null，所以只能通过自旋等待，直到A释放锁后B随后抢到。
+ */
+public class SpinLockDemo {
+    //原子引用线程
+    AtomicReference<Thread> atomicReference = new AtomicReference<>();
+
+    public void myLock() {
+        Thread thread = Thread.currentThread();
+        System.out.println(Thread.currentThread().getName() + "\t come in myLock()");
+        do {
+
+        } while (!atomicReference.compareAndSet(null, thread));
+    }
+
+    public void myUnLock() {
+        Thread thread = Thread.currentThread();
+        atomicReference.compareAndSet(thread, null);
+        System.out.println(Thread.currentThread().getName() + "\t come in myUnLock()");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SpinLockDemo spinLockDemo = new SpinLockDemo();
+        new Thread(() -> {
+            spinLockDemo.myLock();
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            spinLockDemo.myUnLock();
+        }, "AA").start();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        new Thread(() -> {
+            spinLockDemo.myLock();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            spinLockDemo.myUnLock();
+        }, "BB").start();
+    }
+}
