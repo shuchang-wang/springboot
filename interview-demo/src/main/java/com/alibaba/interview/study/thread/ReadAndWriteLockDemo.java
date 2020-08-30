260253839bfb6b39631847a06a6ebb945a578b33
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-08-30 22:34:00 +0800

    读写锁ReentrantReadWriteLock；读锁共享，写锁互斥

---
+++ interview-demo/src/main/java/com/alibaba/interview/study/thread/ReadAndWriteLockDemo.java
@@ -0,0 +1,85 @@
+package com.alibaba.interview.study.thread;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author: WSC
+ * @Create 2020/8/30 21:39
+ * <p>
+ *     class ReentrantReadWriteLock implements ReadAndWrite
+ *
+ * 多个线程同时读取一个资源类没有任何问题，所以为了满足并发量，读取共享资源应该可以同时进行。
+ * 但是，如果有一个线程想去写共享资源，就不应该再有其他线程可以对该资源进行读或写
+ * 小总结：
+ *          读-读能共存
+ *          读-写不能共存
+ *          写-写不能共存
+ * <p>
+ *      写操作：原子+独占，整个过程必须是一个完整的统一体，中间不许被分割，被打断
+ */
+public class ReadAndWriteLockDemo {
+    public static void main(String[] args) {
+        MyCache myCache = new MyCache();
+        for (int i = 0; i < 5; i++) {
+            final int tempInt = i;
+            new Thread(() -> {
+                myCache.put(tempInt + "", tempInt + "");
+            }, String.valueOf(tempInt)).start();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            final int tempInt = i;
+            new Thread(() -> {
+                myCache.get(tempInt + "");
+            }, String.valueOf(tempInt)).start();
+        }
+    }
+}
+
+class MyCache {
+    private volatile Map<String, Object> map = new HashMap<>();
+    private ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+
+    public void put(String key, Object value) {
+        rwLock.writeLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t正在写入：" + key);
+            try {
+                //模拟网络延迟
+                TimeUnit.MILLISECONDS.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            map.put(key, value);
+            System.out.println(Thread.currentThread().getName() + "\t写入完成");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            rwLock.writeLock().unlock();
+        }
+
+    }
+
+    public void get(String key) {
+        rwLock.writeLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "\t正在读取：" + key);
+            try {
+                TimeUnit.MILLISECONDS.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Object result = map.get(key);
+            System.out.println(Thread.currentThread().getName() + "\t读取完成,读取结果：" + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            rwLock.writeLock().unlock();
+        }
+
+    }
+
+}
