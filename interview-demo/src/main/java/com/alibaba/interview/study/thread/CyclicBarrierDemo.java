29d28f157ccef6fac47dc3c8431f7b6ae0997800
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-08-31 23:19:41 +0800

    SemaphoreDemo：信号量；抢车位案例

---
+++ interview-demo/src/main/java/com/alibaba/interview/study/thread/CyclicBarrierDemo.java
@@ -0,0 +1,29 @@
+package com.alibaba.interview.study.thread;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @author: WSC
+ * @Create 2020/8/30 23:21
+ * 聚齐七龙珠召唤神龙
+ */
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7,() -> {
+            System.out.println("*******************七龙珠聚齐，开始召唤神龙*******************");
+        });
+        for (int i = 1; i <= 7; i++) {
+            new Thread(() -> {
+                try {
+                    System.out.println("收集到编号为"+Thread.currentThread().getName()+"的龙珠");
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            },String.valueOf(i)).start();
+        }
+    }
+}
---
+++ interview-demo/src/main/java/com/alibaba/interview/study/thread/SemaphoreDemo.java
@@ -0,0 +1,43 @@
+package com.alibaba.interview.study.thread;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: WSC
+ * @Create 2020/8/31 22:27
+ * SemaphoreDemo：信号量
+ * 抢车位demo
+ */
+public class SemaphoreDemo {
+
+    public static void main(String[] args) {
+
+        //非公平锁
+        Semaphore semaphore = new Semaphore(3);//模拟三个停车位
+        //Semaphore semaphore = new Semaphore(3, false);//模拟三个停车位
+        //公平锁
+       //Semaphore semaphore = new Semaphore(3, true);//模拟三个停车位
+
+        for (int i = 0; i < 6; i++) {//模拟6部车
+            new Thread(() -> {
+                try {
+                    semaphore.acquire();
+                    System.out.println("第" + Thread.currentThread().getName() + "号幸运车主，抢到了车位！");
+                    try {
+                        //暂停3秒钟
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("第" + Thread.currentThread().getName() + "号幸运车主，停了3秒钟离开车位！");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    semaphore.release();
+                }
+            }, String.valueOf(i)).start();
+        }
+
+    }
+}
