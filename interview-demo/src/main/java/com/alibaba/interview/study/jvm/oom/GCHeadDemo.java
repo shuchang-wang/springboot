ed7a02cc7ee8c3b50ab6c2891b8a1e2d4651a184
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-09-09 23:23:17 +0800

    java.lang.OutOfMemoryError: GC overhead limit exceeded

--- interview-demo/src/main/java/com/alibaba/interview/study/jvm/oom/GCHeadDemo.java
+++ interview-demo/src/main/java/com/alibaba/interview/study/jvm/oom/GCHeadDemo.java
@@ -7,10 +7,10 @@ import java.util.List;
  * @author: WSC
  * @Create 2020/9/9 22:37
  * JVM参数配置演示：
- *      -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:MaxDirectMemorySize=5m
- *      Exception in thread "main" java.lang.OutOfMemoryError: GC overhead limit exceeded
- *
- * GC回收时间过长时会抛出OutOfMemoryError。过长的定义是，超过98%的时间用力做GC并且回收了不到2%的堆内存，
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:MaxDirectMemorySize=5m
+ * Exception in thread "main" java.lang.OutOfMemoryError: GC overhead limit exceeded
+ * <p>
+ * GC回收时间过长时会抛出OutOfMemoryError。过长的定义是，超过98%的时间用来做GC并且回收了不到2%的堆内存，
  * 连续多次GC都只回收了不到2%的极端情况下才会抛出。
  * 假如不抛出GC overhead limit exceeded 错误会发生什么情况呢？
  * 那就是GC清理的这么点内存很快会再次填满，迫使GC再次执行。这样就形成恶性循环，
@@ -24,9 +24,10 @@ public class GCHeadDemo {
             while (true) {
                 list.add(String.valueOf(++i).intern());
             }
-        } catch (Exception e) {
-            System.out.println("******************************************i="+i);
+        } catch (Throwable e) {
+            System.out.println("******************************************i=" + i);
             e.printStackTrace();
+            throw e;
         }
     }
 }
