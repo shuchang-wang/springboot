9d32fee292762cd1b1328b56a024568dc2efe3cb
shuchang-wang <wb-wsc730020@alibaba-inc.com>
2020-09-11 00:08:51 +0800

    /**  *  2、-Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParNewGC         （ParNew + Tenured）  *      备注说明：Java HotSpot(TM) 64-Bit Server VM warning: Using the ParNew young collector with the Serial old collector is deprecated and will likely be removed in a future release  *  *      -XX:InitialHeapSize=10485760 -XX:MaxHeapSize=10485760 -XX:+PrintCommandLineFlags -XX:+PrintGCDetails -XX:+UseCompressedClassPointers -XX:+UseCompressedOops -XX:-UseLargePagesIndividualAllocation -XX:+UseParNewGC  *      *********************GCDemo hello  *      [GC (Allocation Failure) [ParNew: 2749K->320K(3072K), 0.0009144 secs] 2749K->989K(9920K), 0.0009751 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]  *      [GC (Allocation Failure) [ParNew: 1340K->1340K(3072K), 0.0000317 secs][Tenured: 5669K->3661K(6848K), 0.0038112 secs] 7010K->3661K(9920K), [Metaspace: 3253K->3253K(1056768K)], 0.0038918 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]  *      [Full GC (Allocation Failure) [Tenured: 4822K->4802K(6848K), 0.0027007 secs] 4822K->4802K(9920K), [Metaspace: 3253K->3253K(1056768K)], 0.0027203 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]  *      Heap  *          par new generation   total 3072K, used 80K [0x00000000ff600000, 0x00000000ff950000, 0x00000000ff950000)  *              eden space 2752K,   2% used [0x00000000ff600000, 0x00000000ff614220, 0x00000000ff8b0000)  *              from space 320K,   0% used [0x00000000ff900000, 0x00000000ff900000, 0x00000000ff950000)  *              to   space 320K,   0% used [0x00000000ff8b0000, 0x00000000ff8b0000, 0x00000000ff900000)  *          tenured generation   total 6848K, used 4802K [0x00000000ff950000, 0x0000000100000000, 0x0000000100000000)  *              the space 6848K,  70% used [0x00000000ff950000, 0x00000000ffe00b40, 0x00000000ffe00c00, 0x0000000100000000)  *          Metaspace       used 3284K, capacity 4496K, committed 4864K, reserved 1056768K  *              class space    used 354K, capacity 388K, committed 512K, reserved 1048576K  *  */

--- interview-demo/src/main/java/com/alibaba/interview/study/jvm/oom/GCDemo.java
+++ interview-demo/src/main/java/com/alibaba/interview/study/jvm/oom/GCDemo.java
@@ -23,6 +23,24 @@ import java.util.Random;
  *      Metaspace       used 3294K, capacity 4496K, committed 4864K, reserved 1056768K
  *          class space    used 354K, capacity 388K, committed 512K, reserved 1048576K
  *
+ *  2、-Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParNewGC         （ParNew + Tenured）
+ *      备注说明：Java HotSpot(TM) 64-Bit Server VM warning: Using the ParNew young collector with the Serial old collector is deprecated and will likely be removed in a future release
+ *
+ *      -XX:InitialHeapSize=10485760 -XX:MaxHeapSize=10485760 -XX:+PrintCommandLineFlags -XX:+PrintGCDetails -XX:+UseCompressedClassPointers -XX:+UseCompressedOops -XX:-UseLargePagesIndividualAllocation -XX:+UseParNewGC
+ *      *********************GCDemo hello
+ *      [GC (Allocation Failure) [ParNew: 2749K->320K(3072K), 0.0009144 secs] 2749K->989K(9920K), 0.0009751 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
+ *      [GC (Allocation Failure) [ParNew: 1340K->1340K(3072K), 0.0000317 secs][Tenured: 5669K->3661K(6848K), 0.0038112 secs] 7010K->3661K(9920K), [Metaspace: 3253K->3253K(1056768K)], 0.0038918 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
+ *      [Full GC (Allocation Failure) [Tenured: 4822K->4802K(6848K), 0.0027007 secs] 4822K->4802K(9920K), [Metaspace: 3253K->3253K(1056768K)], 0.0027203 secs] [Times: user=0.00 sys=0.00, real=0.00 secs]
+ *      Heap
+ *          par new generation   total 3072K, used 80K [0x00000000ff600000, 0x00000000ff950000, 0x00000000ff950000)
+ *              eden space 2752K,   2% used [0x00000000ff600000, 0x00000000ff614220, 0x00000000ff8b0000)
+ *              from space 320K,   0% used [0x00000000ff900000, 0x00000000ff900000, 0x00000000ff950000)
+ *              to   space 320K,   0% used [0x00000000ff8b0000, 0x00000000ff8b0000, 0x00000000ff900000)
+ *          tenured generation   total 6848K, used 4802K [0x00000000ff950000, 0x0000000100000000, 0x0000000100000000)
+ *              the space 6848K,  70% used [0x00000000ff950000, 0x00000000ffe00b40, 0x00000000ffe00c00, 0x0000000100000000)
+ *          Metaspace       used 3284K, capacity 4496K, committed 4864K, reserved 1056768K
+ *              class space    used 354K, capacity 388K, committed 512K, reserved 1048576K
+ *
  */
 public class GCDemo {
     public static void main(String[] args) {
